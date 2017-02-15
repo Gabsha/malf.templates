73dd61a228bd1422ec0d259c43d563b3d52c485c
muschellij2 <muschellij2@gmail.com>
2017-02-15 14:48:05 -0500

    Added filled masks and SS data - hopefully finalized - waiting for CHristos response

--- inst/extdata/make_brains.R
+++ inst/extdata/make_brains.R
@@ -10,7 +10,8 @@ df = data.frame(image = imgs,
                 stringsAsFactors = FALSE)
 df$ss = gsub("[.]nii", "_SS.nii", df$image)
 df$mask = gsub("[.]nii", "_mask.nii", df$image)
-iimg = 1
+iimg = 5
+
 
 for (iimg in seq(nrow(df))) {
   print(iimg)
@@ -18,18 +19,19 @@ for (iimg in seq(nrow(df))) {
   mask_fname = df$mask[iimg]
   img_fname = df$image[iimg]
   label_fname = df$label[iimg]
+
   if (!all(file.exists(ss_fname, mask_fname))) {
     # aimg = antsImageRead(img_fname)
     # # q25 = quantile(aimg[aimg > 0], probs = 0.25)
     # q25 = 0
     # mask = array(aimg > q25, dim = dim(aimg))
     # mask = as.antsImage(mask, reference = aimg)
-    
+
     img = rpi_orient(img_fname)
     img = img$img
-    
+
     label = rpi_orient(label_fname)
-    label = label$img 
+    label = label$img
 
     full_mask = img > 0
 
@@ -39,14 +41,16 @@ for (iimg in seq(nrow(df))) {
                         label = label))
     img = dd$other.imgs$img
     writenii(img, img_fname)
-    
+
     label = dd$other.imgs$label
     writenii(label, label_fname)
-    
+
     mask = label > 0
-    filled = filler(mask, fill_size = 5)
+    filled = filler(mask, fill_size = 5) %>%
+      oMath("FillHoles")
     writenii(filled, mask_fname)
     ss = mask_img(img, filled)
     writenii(ss, ss_fname)
+    # ortho2(img, filled)
   }
 }
